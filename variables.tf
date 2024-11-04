8713c5a991174726f7ec73854373b6cf05980d3b
ccjacobs14 <ccjacobs14@yahoo.com>
2024-11-03 21:06:45 -0500

    updated providers and variables tf files

--- variables.tf
+++ variables.tf
@@ -2,6 +2,7 @@
 variable "bucket_name" {
   description = "Name for the S3 bucket"
   type        = string
+  default = "c14_demo_bucket"
   
 }
 
@@ -11,6 +12,10 @@ variable "bucket_name" {
 variable "tags" {
   description = "A map of tags for the S3 bucket"
   type        = map(string)
+  default = {
+    "Name" = "c14_demo1"
+    "Environment" = "Dev"
+  }
   
   }
 
