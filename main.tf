498e9efc885bc8ae1026eb1dce622a7b7199581b
ccjacobs14 <ccjacobs14@yahoo.com>
2024-11-03 19:11:06 -0500

    modified main.tf & providers.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
-resource "aws_instance" "MyInstance_test" {
+resource "aws_instance" "import_instance" {
     #instance configuration
+    ami = "ami-06b21ccaeff8cd686"
+    instance_type = "t2.micro"
+
+    tags = {
+      Name = "import_instance"
+    }
 }
